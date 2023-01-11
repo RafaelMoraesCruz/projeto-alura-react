f9d0fb42eae7e4b7682669411ddbd8acf029ac9b
RafaelGeorge <rafaelgeorgedemoraescruz@gmail.com>
2023-01-11 14:01:40 -0300

    adicionando tarefa

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,31 +1,32 @@
-import { useRef } from "react";
+import React from "react";
 import Botao from "../Botao";
 import "./style.scss";
+import {tarefas} from '../Lista/index'
 
 function Formulario() {
-  const listaTarefas = [];
-  const tarefa = useRef(null);
-  const tempo = useRef(null);
+  const [state, setState] = React.useState({
+    tarefa: '',
+    tempo: '00:00'
+  })
 
-  function handleButtonClick() {
-    var tarefaCar = [];
-    tarefa.current.focus();
-    tempo.current.focus();
-    tarefaCar.push(tarefa);
-    tarefaCar.push(tempo);
-    listaTarefas.push(tarefaCar);
-    console.log(listaTarefas);
+
+  function adicionarTarefa(evento){
+    setState({
+      tarefa: '',
+      tempo: '00:00'
+    })
   }
 
   return (
-    <form className="novaTarefa">
+    <form className="novaTarefa" onSubmit={adicionarTarefa}>
       <div className="inputContainer">
         <label htmlFor="tarefa">Adicione um novo estudo</label>
         <input
-          ref={tarefa}
           type="text"
           name="tarefa"
           id="tarefa"
+          value={state.tarefa}
+          onChange={evento => setState({...state, tarefa: evento.target.value})}
           placeholder="O que você quer estudar"
           required
         />
@@ -33,17 +34,18 @@ function Formulario() {
       <div className="inputContainer">
         <label htmlFor="tempo">Tempo</label>
         <input
-          ref={tempo}
           type="time"
           step="1"
           name="tempo"
+          value={state.tempo}
+          onChange={evento => setState({...state, tempo: evento.target.value})}
           id="tempo"
           min="00:00:00"
           max="01:30:00"
           required
         />
       </div>
-      <Botao onClick={handleButtonClick} texto='Adicionar'></Botao>
+      <Botao texto='Adicionar'></Botao>
     </form>
   );
 }
